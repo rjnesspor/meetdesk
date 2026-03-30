92314383b737f87569acb121ba506af3c0ad455e
rjnesspor <rjnesspor@gmail.com>
2026-03-30 21:23:29 +0000

    added link to docs in app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, shell } = require('electron');
 Menu.setApplicationMenu(null);
 require('./server.js');
 const path = require('path');
@@ -6,6 +6,11 @@ const path = require('path');
 app.whenReady().then(() => {
     const win = new BrowserWindow({ width: 1200, height: 800, icon: path.join(__dirname, 'assets/meetdesk.ico')});
     win.loadURL('http://localhost:4721');
+
+    win.webContents.setWindowOpenHandler(({ url }) => {
+        shell.openExternal(url);
+        return { action: 'deny' };
+    })
 })
 
 app.on('window-all-closed', () => {
